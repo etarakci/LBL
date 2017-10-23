c7f2521f1633b8832cd762ce9f776ee1533e0ba0
Erendz Tarakci <etarakci@berkeley.edu>
2017-10-22 22:08:31 -0700

    added compression file

---
+++ mountainsort_compress.m
@@ -0,0 +1,31 @@
+
+function ms = msCompress(FILENAME)
+	input = readmda(FILENAME)
+
+	y = null
+	cname = ''
+	fname = ''
+	bname = ''
+
+	for c = 0:.5:1
+		//linearity
+		input = input-(sin(input))*c*input^2
+		cname = num2str(c)
+
+		for f = 32:-2:10
+			//sampling rate
+			p = 32
+			q = f
+			y = resample(input,p,q)
+			fname = num2str(f)
+
+			for b = 16:-2:8
+				//bit depth
+				p = 16
+				q = b
+				% possibly treat matrix as image-numpy.astype
+				bname = num2str(b) 
+				
+				fname = 'c'+ cname + '_f' + fname + '_b' + bname;
+				ms = writemda(input,fname, )
+end
